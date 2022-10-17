6c0e0bec341106a0aaf079f7db97b7efc80b4c63
mzlorence <mzlorence@gmail.com>
2022-10-17 17:24:27 +0200

    message

--- lib/widgets/message_widget.dart
+++ lib/widgets/message_widget.dart
@@ -15,45 +15,50 @@ class _MessageWidgetState extends State<MessageWidget> {
   ];
 
   Widget messageTemplate(message){
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      color: Colors.blueGrey[400],
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              message.user,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          print('well done');
+        });
+      },
+      child: Card(
+        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+        color: Colors.blueGrey[400],
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                message.user,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.black,
+                ),
               ),
-            ),
-            SizedBox(height:6.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  message.last_message,
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.black,
+              SizedBox(height:6.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    message.last_message,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                SizedBox(width:8.0),
-                Text(
-                  message.date_last_message.hour.toString() + ' h',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.black,
+                  SizedBox(width:8.0),
+                  Text(
+                    message.date_last_message.hour.toString() + ' h',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-              ],
-            ),
-
-
-          ],
+                ],
+              ),
+            ],
+          )
         )
       )
     );
