00b54255ae7c6ae11dbd214794258a81c8444fd3
mzlorence <mzlorence@gmail.com>
2022-12-16 20:04:35 +0100

    test profile update

---
+++ test/unit_testing/profile_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:test/test.dart';
+import 'package:skillogue/entities/profile.dart';
+
+void main(){
+  group('Profile', (){
+
+    test('Empty profile', (){
+      final profile = Profile('', '', '', '', '', 1, DateTime.now(), Colors.white,
+          [], []);
+      bool profile_status = profile.isEmptyProfile();
+      expect(profile_status, true);
+    });
+
+    test('Not empty profile', (){
+      final profile = Profile('Test1', 'Test1', 'Test1', 'Test1', 'Test1', 1, DateTime.now(), Colors.white,
+          ['test1'], ['test1']);
+      bool profile_status = profile.isEmptyProfile();
+      expect(profile_status, false);
+    });
+  });
+
+}
